ae9525b27571fa432fd38149ba8db091716df1fd
Aditi Shukla <aditi.shukla0111@gmail.com>
2024-05-08 00:19:05 +0530

    quiz app

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,49 @@
+import React, { useState,useEffect} from 'react'
+
+function QuestionTimer({timeout,onTimeout}) {
+  
+
+const [remainingTime,setRemainingTime]=useState(timeout);
+// setTimeout(onTimeout,timeout)
+//Not an effect that would require useEffect as 1. we are not facing danger of infinite loop.
+//2. no ref involved 
+//3.and I am not updating component state here.
+ 
+//** 
+useEffect(()=>{
+  setTimeout(onTimeout,timeout)
+},[timeout,onTimeout])
+//in dependency,need to add any prop or state value used inside effect function 
+//note=> onTimeout is a function
+
+
+
+
+// setInterval(()=>{
+//   setRemainingTime(prev=>prev-100)
+// },100)
+
+//Now this would create infinite loop as we are updating state=> that would re-execute the component=>new setInterval will be created and we will set the state again=>we will quickly have multiple intervals running
+  
+useEffect(()=>{
+  setInterval(()=>{
+    setRemainingTime(prev=>prev-100)
+  },100)
+},[]);
+//every 100 miliseconds setInterval will run which in turn re-create setTimeOut 
+//as comp will re-render =>and quickly, we will have multiple setTimeOuts. 
+//Hence setTimeOut needs to be wrapped in useEffect. **
+
+return (
+    <progress  id="question-time" max={timeout} value={remainingTime}/>
+  )
+}
+
+export default QuestionTimer;
+
+
+//every question has a timer for say 15 seconds and once the timer is up,
+//move to next question and save the skiped question's answer as null.
+
+//WE have to inform parent comp that the timer for current question has expired.
+//Therefore, we will accept a function prop which will fire once the timer expires.
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,86 @@
+//want to use this component to show the currently active question to the user.
+//and when the user clicks on a question, I want to switch to a different question.
+//so this comp is responsible for switching questions and for registering answers.
+import React from 'react'
+import {useState} from 'react'
+import QUSETIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from './QuestionTimer';
+function Quiz() {
+//STORE index of current question
+// const [activeQuestionIndex,setActiveQuestionIndex]=useState(0); -but this is a redundant way . You can find the indexof questopn by using userAnswers Array
+//---------------------
+//store option selected by users in an array
+const [userAnswers,setUserAnswers]=useState([]);
+
+const activeQuestionIndex=userAnswers.length;
+// If we have stored two answers for question 1 and 2 at index 0,1  
+//then length of userAnswers is userAnswer.length=2
+// and  we have to show the third question(question at index 2).
+//Hence, activeQuestionIndex=2=userAnswer.length
+
+
+function handleSelectedAnswer(selectedOption)
+{//when state is updated,entire state gets overrwitten.
+    //Therefore, always copy the previous data and then add new data point.
+    setUserAnswers((prev)=>
+    {return [...prev,selectedOption]});
+
+}
+
+
+//These two codes should only be executed if there is more questions left.
+//bcoz once <activeQuestionIndex> reaches the last index.Beyound that it will become undefined.
+//TypeError: Cannot read properties of undefined 
+//Therefore, moving them  below <if(quizIsComplete)>
+
+//Going to use "sort method" which updates the actual array.Therefore, first copy it using spread operator
+
+// const shuffledAnswers=[...QUSETIONS[activeQuestionIndex].answers];
+
+//this will shuffle the array
+// shuffledAnswers.sort(()=>Math.random()-0.5);
+
+//THe user has answered all the questions if activeQuestionIndex=== QUESTIONS.length 
+const quizIsComplete=activeQuestionIndex === QUSETIONS.length;
+
+//Show different content once all quiz questions are over.
+if(quizIsComplete)
+    {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon" />
+                <h2>Quiz Complete !</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers=[...QUSETIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(()=>Math.random()-0.5);
+
+  return (
+    <><div id="quiz">
+        <div id="question">
+        <QuestionTimer  timeout={10000} onTimeout={()=>{
+            handleSelectedAnswer(null)
+        }}/>
+            <h2>{QUSETIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {
+                 shuffledAnswers.map(element=>
+                    //  QUSETIONS[activeQuestionIndex].answers.map(...) => changed to shuffledAnswers.map(...)
+                        <li key={element} className='answer'>
+                        <button onClick={()=>handleSelectedAnswer(element)}>{element}</button>
+                            {/* <button onClick={handleSelectedAnswer}>{element}</button> */}
+                            {/* Not enough to point at handleSecetedAnswer fun. */}
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Quiz

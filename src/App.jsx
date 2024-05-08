1386f4efa5cf174ce9b1788ce78090f46b7f837a
Aditi Shukla <aditi.shukla0111@gmail.com>
2024-05-08 22:02:46 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
-import Quiz from "./components/Quiz"
+import Quiz from "./components/Quiz";
 function App() {
-    return (<><Header/>
-    <main>    <Quiz /></main>
-
-    </>)
+  return (
+    <>
+      <Header />
+      <main>
+        {" "}
+        <Quiz />
+      </main>
+    </>
+  );
 }
 
 export default App;

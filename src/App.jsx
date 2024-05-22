f7745455680da5ca3762f1b1c8d18aa3a7c75ace
Aditi Shukla <aditi.shukla0111@gmail.com>
2024-05-22 11:39:38 +0530

    part4

--- src/App.jsx
+++ src/App.jsx
@@ -14,44 +14,14 @@ function App() {
 
 export default App;
 
-//Part3: do <git checkout part3>
-//So right now, what is happening is that the moment student chooses an answer, we instantly go to next question
-//But Now: I want to hightlight the ans chosen by the user.
-//and then after 1 sec=>
-//Highlight the ans red->if wrong
-//Highlight the ans green-> if correct
-//and after 2 seconds, next question should be loaded.
-//To implement this=>some changes are required
-//in Quiz.jsx=> need to change <handleSelectAnswer>
-// EXAMPLE
-// userAnswers.length = 5
-// answered = ""
-// const activeQuestionIndex = answered ==='' // answered = "" -- TRUE
-//     ? userAnswer.length // TRUTHY VALUE <=== RETURN THIS!
-//     : userAnswer.length - 1;  // FALSY VALUE
-// // activeQuestionIndex = 5
-// User selects an answer
 
-// setAnswered('answered');
-// setUserAnswer(prevAnswers => [...prevAnswers, answer]); // ANSWERS .length GROWS BY 1
-// // answered = 'answered'
-// // userAnswer.length = 6
+//PART4:
+// But now you will notice as your selected button changes color=>it jumps around.
+//This is because we are using a state to help change color.Hence as state 
+//makes comp re-render.The options gets shuffled at every re-rendeR BCOZ OF THIS CODE👇
 
-// state updates, component re-evaluates this line👇
-// const activeQuestionIndex = answered === '' // answered = "answered" -- FALSE
-//     ? userAnswer.length // TRUTHY VALUE
-//     : userAnswer.length - 1;  // FALSY VALUE <=== RETURN THIS!
-// // activeQuestionIndex = 6 - 1 = 5
-// after 2 seconds
+// const shuffledAnswers = [...QUSETIONS[activeQuestionIndex].answers];
+// shuffledAnswers.sort(() => Math.random() - 0.5);
 
-// setAnswered('');
-// // answered = ''
-
-
-// state updates, component re-evaluates this line👇
-// const activeQuestionIndex = answered === '' // answered = "" -- TRUE
-//     ? userAnswer.length // TRUTHY VALUE <=== RETURN THIS!
-//     : userAnswer.length - 1;  // FALSY VALUE
-// // activeQuestionIndex = 6
-// New question renders!
-
+//Hence your chosen option jumps around as it changes color.
+//Therefore, time to split the Quiz code to solve this problem.
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -129,5 +129,12 @@ export default Quiz;
 // But now you will notice as your selected button changes color=>it jumps around.
 //This is because we are using a state to help change color.Hence as state makes comp re-render.The options gets shuffled at every re-render.
 //Hence your chosen option jumps around as it changes color.
-//Therefore, time to split the Quiz code to solve this problem.
-//do <git checkout part4>
+
+
+
+
+//
+// if (answerState === "") {
+//   shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
+//   shuffledAnswers.current.sort(() => Math.random() - 0.5);
+// }

be5911afdb71001d460c9ac9083028cc61c5f0b3
Aditi Shukla <aditi.shukla0111@gmail.com>
2024-05-29 00:03:44 +0530

    quiz app

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,52 +4,48 @@ import Answers from "./Answers";
 import QUSETIONS from "../questions";
 import { useState } from "react";
 function Question({
-  QuestionIndex,
+  QuestionIndex,//for access to activeQuestionIndex.From this we are evaluation isCorrect.
 
-  onSelectAnswer,
- 
-  onSkip,
+  onSelectAnswer, //to increasse the length of array by 1
+
+  onSkip,  //Quiz.jsx->Question.jsx-> QuestionTimer.jsx.Remember, we have made Question.jsx so that we use one key prop .
+  //and we want answers component and QuestionTimer Component to be re-created for each and every question 
 }) {
-
-
-  const [answer,setAnswer]=useState({
-    selectedAnswer:'',
-    isCorrect:null
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
   });
 
-  function handleChoosenAnswer(answer){
+  function handleChoosenAnswer(answer) { //answer coming from 'onSelect' in Answers component.
     setAnswer({
-    selectedAnswer:answer,
-    isCorrect:null
-    })
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
 
     //after 1sec, set 'isCorrect' to true or false,Hence show the user if their ans was right or wrong.
-    setTimeout(()=>{
-      setAnswer({
-        selectedAnswer:answer,
-        isCorrect:QUSETIONS[QuestionIndex].answers[0]===answer
-        })
-    //AFTER ADDITIONAL 2 SEC=>MOVE TO THE NEXT QUESTION
-//we are writing this setTimeOut here so that we don't instantly move away.
     setTimeout(() => {
-     onSelectAnswer(answer); //This would now increase the length  of array by 1  in Quiz.jsx. Hence we do not need 
-// this code is the only thing controlling the length of array <userAnswers>
-    }, 2000);
-    },1000)
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUSETIONS[QuestionIndex].answers[0] === answer,
+      });
+      //AFTER ADDITIONAL 2 SEC=>MOVE TO THE NEXT QUESTION
+      //we are writing this setTimeOut here so that we don't instantly move away.
+      setTimeout(() => {
+        onSelectAnswer(answer); //This would now increase the length  of array by 1  in Quiz.jsx.
+        // this code is the only thing controlling the length of array <userAnswers>
+      }, 2000);
+    }, 1000);
   }
 
-
-
-let StateofAns='';
-if(answer.selectedAnswer && answer.isCorrect !== null )
-  {//we have marked our answer and we have not evaluated whether our ans in correct or not.
-    StateofAns=answer.isCorrect?'correct':'wrong'; //'correct(green)'- 'wrong(red)' are css classNames.
-  }
- else if (answer.selectedAnswer){//we have marked the answer and isCorrect is null. WE have not yet evaluated if our answer is correct or not
-    StateofAns='answered';
+  let StateofAns = "";
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    //we have marked our answer and we have not evaluated whether our ans in correct or not.
+    StateofAns = answer.isCorrect ? "correct" : "wrong"; //'correct(green)'- 'wrong(red)' are css classNames.
+  } else if (answer.selectedAnswer) {
+    //we have marked the answer and isCorrect is null. WE have not yet evaluated if our answer is correct or not
+    StateofAns = "answered";
   }
 
- 
   return (
     <div id="question">
       <QuestionTimer onTimeout={onSkip} timeout={40000} />
@@ -59,8 +55,7 @@ if(answer.selectedAnswer && answer.isCorrect !== null )
       <Answers
         onSelect={handleChoosenAnswer} //element argument will automatically be passed to handleChoosenAnswer.So it is enough to simply point to it.
         answers={QUSETIONS[QuestionIndex].answers}
-    
-        selectedAnswer={answer.selectedAnswer} 
+        selectedAnswer={answer.selectedAnswer}
         answerState={StateofAns}
       />
     </div>
@@ -68,4 +63,3 @@ if(answer.selectedAnswer && answer.isCorrect !== null )
 }
 
 export default Question;
-
